3556235679bb262983b4aeecd2b5d912a9b2650d
Raghu <khajuriaraghu0@gmail.com>
2025-05-22 04:08:48 +0530

    protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,58 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import { Navigate } from "react-router-dom";
+// import axios from "axios";
+// import { useSelector, useDispatch } from "react-redux";
+// import { hideLoading, showLoadings } from "../redux/features/alertSlice";
+// import { setUser } from "../redux/features/userSlice";
+
+// function ProtectedRoute({ children }) {
+//   const dispatch = useDispatch();
+//   const { user } = useSelector((state) => state.user);
+
+//   // get user
+//   const getUser = async () => {
+//     try {
+//       // dispatch(showLoadings());
+//       const res = await axios.post(
+//         "https://vercel-backend-henna.vercel.app/api/v1/user/getUserData",
+//         {
+//           token: localStorage.getItem("token"),
+//         },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem("token")}`,
+//           },
+//         }
+//       );
+//       dispatch(hideLoading());
+//       if (res.data.success) {
+//         dispatch(setUser(res.data.data));
+//       } else {
+//         <Navigate to="/login" />;
+//         localStorage.clear();
+//       }
+//     } catch (error) {
+//       localStorage.clear();
+//       dispatch(hideLoading());
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (!user) {
+//       getUser();
+//     }
+//   }, [user, getUser]);
+
+//   if (localStorage.getItem("token")) {
+//     return children;
+//   } else {
+//     return <Navigate to="/login" />;
+//   }
+// }
+
+// export default ProtectedRoute;
+
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
@@ -8,16 +62,14 @@ import { setUser } from "../redux/features/userSlice";
 function ProtectedRoute({ children }) {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const [loading, setLoading] = useState(true);
 
-  // get user
   const getUser = async () => {
     try {
-      // dispatch(showLoadings());
+      dispatch(showLoadings());
       const res = await axios.post(
         "https://vercel-backend-henna.vercel.app/api/v1/user/getUserData",
-        {
-          token: localStorage.getItem("token"),
-        },
+        {},
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -28,20 +80,27 @@ function ProtectedRoute({ children }) {
       if (res.data.success) {
         dispatch(setUser(res.data.data));
       } else {
-        <Navigate to="/login" />;
         localStorage.clear();
       }
     } catch (error) {
       localStorage.clear();
       dispatch(hideLoading());
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     if (!user) {
       getUser();
+    } else {
+      setLoading(false);
     }
-  }, [user, getUser]);
+  }, [user]);
+
+  if (loading) {
+    return null; // optional: return a spinner if needed
+  }
 
   if (localStorage.getItem("token")) {
     return children;
@@ -51,3 +110,4 @@ function ProtectedRoute({ children }) {
 }
 
 export default ProtectedRoute;
+
